250a300e29a59a3400d7e15c4e630826187c7e53
Pinguin <maxsunkovsky@gmail.com>
2024-11-18 20:33:42 +0100

    home PC patch

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pyright" }
+local servers = { "html", "cssls", "pyright", "julia-lsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
