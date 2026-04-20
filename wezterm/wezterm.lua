103af68ad0050f3a3b4a6961978e61344362fb63
Ping-uin <maxsunkovsky@gmail.com>
2026-04-21 00:05:09 +0200

    refactor: cleaner status bar code and new theme/colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.adjust_window_size_when_changing_font_size = false
 -- font and scheme stuff
 config.font = wezterm.font({ family = 'Julia Mono' })
 config.font_size = 12
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = 'Whimsy'
 -- always use zsh when using wez
 config.default_prog = { "zsh" }
 config.keys = {
@@ -33,63 +33,61 @@ config.keys = {
 }
 
 wezterm.on("update-right-status", function(window, pane)
-	-- kanagawa hexcodes
-	local colors = {
-		red = "#E82424", -- kanagawa red
-		orange = "#FF9E3B", -- kanagawa orange
-		green = "#98BB6C", -- kanagawa green
-		blue = "#7E9CD8", -- kanagawa blue
-		gray = "#727169", -- kanagawa gray (für Trenner)
-	}
+    -- Charm.land inspirierte Farbpalette
+    local colors = {
+        pink   = "#F25D94", -- Typisches Charm-Pink (für Uhrzeit)
+        purple = "#7D56F4", -- Charm-Lila (für vollen Akku)
+        cyan   = "#00ADD8", -- Charm-Cyan (für Kalenderwoche)
+        green  = "#04B575", -- Charm-Mint (für Wochentag)
+        gray   = "#6B6B6B", -- Dezentes Grau (für Trenner)
+        red    = "#E84033", -- Warnrot (für fast leeren Akku)
+        orange = "#FF9400", -- Warnorange (für halben Akku)
+    }
 
-	local elements = {}
+    local elements = {}
 
-	-- battery info
-	local battery_info = ""
-	for _, b in ipairs(wezterm.battery_info()) do
-		local battery_pct = b.state_of_charge * 100
-		local battery_icon = "󰁹" -- nerd font
-		local battery_color = colors.blue
-		if battery_pct < 25 then
-			battery_icon = "󰂎"
-			battery_color = colors.red
-		elseif battery_pct < 50 then
-			battery_icon = "󰁿"
-			battery_color = colors.orange
-		elseif battery_pct < 75 then
-			battery_icon = "󰂀"
-			battery_color = colors.green
-		end
-		battery_info = battery_icon .. " " .. string.format("%.0f%%", battery_pct)
+    -- Hilfsfunktionen für sauberen Code (DRY-Prinzip)
+    local function add_text(color, text)
+        table.insert(elements, { Foreground = { Color = color } })
+        table.insert(elements, { Text = text })
+    end
 
-		table.insert(elements, { Foreground = { Color = battery_color } })
-		table.insert(elements, { Text = battery_info })
-	end
+    local function add_separator()
+        if #elements > 0 then
+            add_text(colors.gray, "  •  ")
+        end
+    end
 
-	-- weekday
-	local weekday = wezterm.strftime("%a") -- Mon, Tue, Wed, Thu, Fri, Sat, Sun
-	if #elements > 0 then
-		table.insert(elements, { Foreground = { Color = colors.gray } })
-		table.insert(elements, { Text = "  •  " })
-	end
-	table.insert(elements, { Foreground = { Color = colors.green } })
-	table.insert(elements, { Text = "󰸗 " .. weekday }) -- calendar icon + weekday
+    -- Batterie
+    for _, b in ipairs(wezterm.battery_info()) do
+        local battery_pct = b.state_of_charge * 100
+        local battery_icon = "󰁹"
+        local battery_color = colors.purple -- Standardfarbe ist jetzt das Charm-Lila
 
-	-- calendar week
-	local week_number = wezterm.strftime("%V") -- week number
-	table.insert(elements, { Foreground = { Color = colors.gray } })
-	table.insert(elements, { Text = "  •  " })
-	table.insert(elements, { Foreground = { Color = colors.orange } })
-	table.insert(elements, { Text = "󰨲 " .. "KW" .. week_number }) -- week icon + week number
+        if battery_pct < 25 then
+            battery_icon, battery_color = "󰂎", colors.red
+        elseif battery_pct < 50 then
+            battery_icon, battery_color = "󰁿", colors.orange
+        elseif battery_pct < 75 then
+            battery_icon, battery_color = "󰂀", colors.green
+        end
 
-	-- curr time
-	local time = wezterm.strftime("%H:%M")
-	table.insert(elements, { Foreground = { Color = colors.gray } })
-	table.insert(elements, { Text = "  •  " })
-	table.insert(elements, { Foreground = { Color = colors.red } })
-	table.insert(elements, { Text = "󰥔 " .. time .. " " })
+        add_text(battery_color, string.format("%s %.0f%%", battery_icon, battery_pct))
+    end
 
-	window:set_right_status(wezterm.format(elements))
+    -- Wochentag
+    add_separator()
+    add_text(colors.green, "󰸗 " .. wezterm.strftime("%a"))
+
+    -- Kalenderwoche
+    add_separator()
+    add_text(colors.cyan, "󰨲 KW" .. wezterm.strftime("%V"))
+
+    -- Uhrzeit
+    add_separator()
+    add_text(colors.pink, "󰥔 " .. wezterm.strftime("%H:%M") .. " ")
+
+    window:set_right_status(wezterm.format(elements))
 end)
 
 return config
