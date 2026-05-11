ac642a9b912e4d33e42f4eaba58d22bc7af4b790
Maximilian_Sunkovsky <maximilian.sunkovsky@bright-testing.de>
2026-05-11 10:00:41 +0200

    feat: new theme and window size on startup

--- doom/config.el
+++ doom/config.el
@@ -33,7 +33,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -43,6 +43,12 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; ----- CUSTOM -----
+;; Initiale Fenstergröße (full)
+;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;; Initiale Fenstergröße (custom)
+(add-to-list 'default-frame-alist '(width . 140))
+(add-to-list 'default-frame-alist '(height . 55))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `with-eval-after-load' block, otherwise Doom's defaults may override your
